2ddd13afd34c4154a3e58aae50dbc063d2c67413
pOlegV <oleg-1@yandex.ru>
2022-07-19 11:03:33 +0300

    Add Test csv

--- src/test/java/SQRServiceTest.java
+++ src/test/java/SQRServiceTest.java
@@ -1,8 +1,19 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.sqr.Dz_5_1.service.SQRService;
 
 public class SQRServiceTest {
+    @ParameterizedTest
+    @CsvFileSource (files = "src/test/resources/numberSqr.csv")
+    public void enterTheBorder(int expected, int lowerBound, int upperBound) {
+        SQRService service = new SQRService();
+        int actual = service.numberOfSqr(lowerBound, upperBound);
+        Assertions.assertEquals(expected, actual);
+    }
+    /*
     @Test
     public void enterTheBorder() {
         int expected = 3;
@@ -26,4 +37,5 @@ public class SQRServiceTest {
         int actual = service.numberOfSqr(100, 9801);
         Assertions.assertEquals(expected, actual);
     }
+    */
 }
